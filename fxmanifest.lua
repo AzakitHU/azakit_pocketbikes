60d985110dbbb1c95c29e363e5831f8d4c68f6d1
Azakit <155274165+AzakitHU@users.noreply.github.com>
2025-01-07 02:18:38 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,5 +18,10 @@ server_scripts {
 }
 
 shared_scripts {
-	'@es_extended/imports.lua',   -- Uncomment if using ESX
+	'@es_extended/imports.lua',
+  --  '@qb-core/shared/items.lua',
+}
+dependencies {
+    'es_extended',
+    --'qb-core'
 }
